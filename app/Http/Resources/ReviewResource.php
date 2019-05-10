79c0faf304b9645b8ae08001f141d81d3150a4f3
Rogene Cris Violeta <rogenecris.violeta@flatworld.com.ph>
2019-05-11 04:34:47 +0800

    Review resource. Displayed all reviews for a specific product

--- app/Http/Resources/ReviewResource.php
+++ app/Http/Resources/ReviewResource.php
@@ -6,14 +6,18 @@ use Illuminate\Http\Resources\Json\JsonResource;
 
 class ReviewResource extends JsonResource
 {
-    /**
-     * Transform the resource into an array.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return array
-     */
-    public function toArray($request)
-    {
-        return parent::toArray($request);
-    }
+  /**
+   * Transform the resource into an array.
+   *
+   * @param  \Illuminate\Http\Request  $request
+   * @return array
+   */
+  public function toArray($request)
+  {
+    return [
+      'customer' => $this->customer,
+      'body' => $this->review,
+      'star' => $this->star
+    ];
+  }
 }
