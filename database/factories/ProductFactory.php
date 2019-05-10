8eff6994ef98d69d3ada2e756a1aec701dbf6dfa
Rogene Cris Violeta <rcvioleta13@gmail.com>
2019-05-10 18:02:50 +0800

    Created Relationship between Products and Reviews

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -4,6 +4,10 @@ use Faker\Generator as Faker;
 
 $factory->define(App\Model\Product::class, function (Faker $faker) {
     return [
-        //
+        'name' => $faker->word,
+        'detail' => $faker->text(20),
+        'price' => $faker->numberBetween(30, 200),
+        'stock' => $faker->randomDigit,
+        'discount' => $faker->numberBetween(2, 30)
     ];
 });
